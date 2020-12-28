8b6eef70d984c05f456d98557b60594b1820e6b8
rishipurwar1 <rishipurwar007@gmail.com>
2020-12-28 13:51:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const buttons = document.querySelectorAll('button');
+// const contents = document.querySelectorAll('.content');
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//         const sibling = e.target.nextElementSibling;
+//         sibling.classList.toggle('active');
+//         contents.forEach(content => {
+//             if(content !== sibling){
+//                 if(content.className === 'content active'){
+//                     content.classList.remove('active');
+//                 }
+//             }
+//         });
+//     });
+// });
